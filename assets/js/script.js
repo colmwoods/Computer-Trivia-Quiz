806577c9e50b54bdab6ff31fa00cbbf9f39692f8
Colm Woods <colmwoods13@gmail.com>
2025-08-13 09:15:39 +0100

    Add helpful comments in for each const varable

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,20 @@
-const page1 = document.getElementById("page1");
-const page2 = document.getElementById("page2");
-const quiz = document.getElementById("quiz");
-const end = document.getElementById("end");
+const page1 = document.getElementById("page1"); // Start Page
+const page2 = document.getElementById("page2"); // Quiz Page
+const quiz = document.getElementById("quiz"); // Quiz Section
+const end = document.getElementById("end"); // End Page
 
-const difficultySelect = document.getElementById("difficulty");
-const ontoPage2 = document.getElementById("ontoPage2");
-const numberOfQuestions = document.getElementById("numberOfQuestions");
-const startQuizButton = document.getElementById("startQuiz");
-const questionNumber = document.getElementById("questionNumber");
-const score = document.getElementById("score");
-const incorrect = document.getElementById("incorrect");
-const questionsElement = document.getElementById("questions");
-const choicesElement = document.getElementById("choices");
-const NextQuestion = document.getElementById("nextQuestion");
-const totalScore = document.getElementById("total");
-const againButton = document.getElementById("againBtn");
+const difficultySelect = document.getElementById("difficulty"); // Difficulty Selection
+const ontoPage2 = document.getElementById("ontoPage2"); // Button to Proceed to Number of Questions Page
+const numberOfQuestions = document.getElementById("numberOfQuestions"); // Number of Questions Selection Min 5 Max 25
+const startQuizButton = document.getElementById("startQuiz"); // Start Quiz Button
+const questionNumber = document.getElementById("questionNumber"); // Question Number Display
+const score = document.getElementById("score"); // Score Display
+const incorrect = document.getElementById("incorrect"); // Incorrect Answers Display
+const questionsDisplay = document.getElementById("questions"); // Questions Display
+const choicesElement = document.getElementById("choices"); // Choices Display
+const NextQuestion = document.getElementById("nextQuestion"); // Next Question Button
+const totalScore = document.getElementById("total"); // Total Score Display
+const againButton = document.getElementById("againBtn"); // Play Again Button
 
 
 function showPage(sectionId) {
