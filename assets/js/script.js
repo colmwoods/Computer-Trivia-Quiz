6cc43dc873411a4e20d3c1ca358f2b5b2f963e8c
Colm Woods <colmwoods13@gmail.com>
2025-08-13 09:11:35 +0100

    Give all of my ids in index.html a constant varable

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,22 @@
+const page1 = document.getElementById("page1");
+const page2 = document.getElementById("page2");
+const quiz = document.getElementById("quiz");
+const end = document.getElementById("end");
+
+const difficultySelect = document.getElementById("difficulty");
+const ontoPage2 = document.getElementById("ontoPage2");
+const numberOfQuestions = document.getElementById("numberOfQuestions");
+const startQuizButton = document.getElementById("startQuiz");
+const questionNumber = document.getElementById("questionNumber");
+const score = document.getElementById("score");
+const incorrect = document.getElementById("incorrect");
+const questionsElement = document.getElementById("questions");
+const choicesElement = document.getElementById("choices");
+const NextQuestion = document.getElementById("nextQuestion");
+const totalScore = document.getElementById("total");
+const againButton = document.getElementById("againBtn");
+
+
 function showPage(sectionId) {
 }
 
